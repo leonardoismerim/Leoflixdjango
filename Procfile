892be4a2fd4ea2008a83dc3423fad2056eec810d
leonardoismerim <leonardoismerim3@gmail.com>
2026-09-12 19:56:09 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn leoflix.wsgi --log-file -
---
+++ filme/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+# Create your tests here. - url - view - template sempre que for criar um pagina tem que fazer essas 3 coisas 
+
