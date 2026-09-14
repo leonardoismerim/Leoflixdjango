89a7086afca1400e14225f417b3399ec0b8b06ce
leonardoismerim <leonardoismerim3@gmail.com>
2026-09-14 16:57:23 -0300

    token-csrf

--- leoflix/settings.py
+++ leoflix/settings.py
@@ -20,6 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY =TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://leoflixdjango-production.up.railway.app']
+else:
+    SECRET_KEY = 'django-insecure-7bq-f#$o40&b61+reaemvgnko6j95!r9o$x%_5!7$gy8v@w%58'
 
 
 SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin" # para pode rodar os videos do youtube
@@ -27,7 +33,7 @@ SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin" # para pode rodar os 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["leoflixdjango-production.up.railway.app", "localhost", "127.0 0.1"]
 
 
 # Application definition
